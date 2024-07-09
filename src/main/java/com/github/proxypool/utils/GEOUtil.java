c7671a01f1e94c3adca1ae8c24fbff703aa60f13
tgscan-dev <tgscan.xyz@outlook.com>
2024-07-09 15:35:14 +0800

    fix jar path

--- src/main/java/com/github/proxypool/utils/GEOUtil.java
+++ src/main/java/com/github/proxypool/utils/GEOUtil.java
@@ -3,27 +3,38 @@ package com.github.proxypool.utils;
 import com.maxmind.geoip2.DatabaseReader;
 import com.maxmind.geoip2.exception.GeoIp2Exception;
 import com.maxmind.geoip2.model.CityResponse;
-import java.io.File;
+
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetAddress;
 import java.util.Optional;
 
 public class GEOUtil {
 
-  private static File database = null;
+  private static DatabaseReader dbReader = null;
 
   static {
-    var path = GEOUtil.class.getClassLoader().getResource("GeoLite2-City.mmdb").getPath();
-    database = new File(path);
+    try (InputStream databaseStream = GEOUtil.class.getClassLoader().getResourceAsStream("GeoLite2-City.mmdb")) {
+      if (databaseStream != null) {
+        dbReader = new DatabaseReader.Builder(databaseStream).build();
+      } else {
+        throw new IllegalStateException("GeoLite2-City.mmdb not found in resources");
+      }
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to initialize DatabaseReader", e);
+    }
   }
 
   public static Optional<CityResponse> getCityResponse(String ip) {
     try {
-      DatabaseReader dbReader = new DatabaseReader.Builder(database).build();
       InetAddress ipAddress = InetAddress.getByName(ip);
-      return Optional.of(dbReader.city(ipAddress));
+      return Optional.ofNullable(dbReader.city(ipAddress));
     } catch (IOException | GeoIp2Exception e) {
       return Optional.empty();
     }
+  }
+
+  public static void main(String[] args) {
+    System.out.println(getCityResponse("11.23.2.3"));
   }
 }
