4f1199f2e9ddb9270e45dd7b3a47a5a0d50613cc
tgscan-dev <tgscan.xyz@outlook.com>
2024-07-09 14:28:42 +0800

    Fix url

--- src/main/java/com/github/proxypool/ProxyPoolApplication.java
+++ src/main/java/com/github/proxypool/ProxyPoolApplication.java
@@ -1,9 +1,14 @@
 package com.github.proxypool;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@OpenAPIDefinition(
+		servers = @Server(url = "/")
+)
 @SpringBootApplication
 @EnableScheduling
 public class ProxyPoolApplication {
